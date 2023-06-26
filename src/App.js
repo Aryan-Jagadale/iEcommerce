0172a86833371984e508d2ab3aa74ac3be438822
Aryan Jagadale <aryansjagadale@gmail.com>
2023-06-26 13:40:41 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header"
+import Home from "./components/Home"
+import Cart from "./components/Cart"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
